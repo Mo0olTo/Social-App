33e26d30d230391673c2bdac5695552f2e9b501f
Mo0olTo <mostafa.allam.de@gmail.com>
2025-04-14 22:53:35 +0200

    edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
-import { RenderMode } from '@angular/ssr';
+
 import { Routes } from '@angular/router';
 import { AuthComponent } from './layouts/auth/auth/auth.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './layouts/main/main/main.component';
-import { ChangepasswordComponent } from './pages/changepassword/changepassword.component';
+
 import { loginGuard } from './core/guards/login.guard';
 import { loggedGuard } from './core/guards/logged.guard';
 import { PostdetailsComponent } from './pages/postdetails/postdetails.component';
@@ -23,9 +23,7 @@ export const routes: Routes = [
         {path:'home' , loadComponent:()=>import('./pages/home/home.component').then((c)=>c.HomeComponent) , title:"Home"},
         {path:'changepassword' ,loadComponent:()=>import('./pages/changepassword/changepassword.component').then((c)=>c.ChangepasswordComponent) , title:'Change-Password'},
         {path:'myposts' ,loadComponent:()=>import('./pages/myposts/myposts.component').then((c)=>c.MypostsComponent) , title:'My Posts'},
-        {path:'userinfo' ,loadComponent:()=>import('./pages/userinfo/userinfo.component').then((c)=>c.UserinfoComponent) , title:'User-Profile' ,children:[
-            
-        ]},
+        {path:'userinfo' ,loadComponent:()=>import('./pages/userinfo/userinfo.component').then((c)=>c.UserinfoComponent) , title:'User-Profile' },
         {path:'post/:id' , component:PostdetailsComponent , data:{ rendermode: 'no-prerender' }},
         {path:'**' , loadComponent:()=>import('./pages/notfound/notfound.component').then((c)=>c.NotfoundComponent)}
 
